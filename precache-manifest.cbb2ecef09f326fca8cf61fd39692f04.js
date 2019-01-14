c666efb8ecd6a04bad5af59cb5931f1c0fdce302
Rushabh Nagda <rushabhnagda11@gmail.com>
2019-01-14 19:52:33 +0530

    Updates

---
+++ precache-manifest.cbb2ecef09f326fca8cf61fd39692f04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d21a56e998dc68750488",
+    "url": "/pd-nsfw/static/js/runtime~main.d21a56e9.js"
+  },
+  {
+    "revision": "d6661f1ac2994a48543d",
+    "url": "/pd-nsfw/static/js/main.d6661f1a.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/pd-nsfw/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "d6661f1ac2994a48543d",
+    "url": "/pd-nsfw/static/css/main.fa9f4cb8.chunk.css"
+  },
+  {
+    "revision": "a615f16ea00999f7d6269aa726321126",
+    "url": "/pd-nsfw/index.html"
+  }
+];
