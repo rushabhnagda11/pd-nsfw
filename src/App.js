b661e45d273473ff3cdd845935c8c865be0ed68e
Rushabh Nagda <rushabhnagda11@gmail.com>
2019-01-14 18:45:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const data = [
+  {
+    "urls" : ["1","2","3","1","2","3"],
+    "title" : "Porn",
+  },
+  {
+    "urls" : ["1","2","3","1","2","3"],
+    "title" : "Explicit Nudity",
+  },
+  {
+    "urls" : ["1","2","3","1","2","3"],
+    "title" : "Animated Porn",
+  },
+  {
+    "urls" : ["1","2","3","1","2","3"],
+    "title" : "Gore",
+
+  },
+  {
+    "urls" : ["1","2","3","1","2","3"],
+    "title" : "Suggestive Nudity",
+  }
+]
+
+class ImageHolder extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      url: this.props.url,
+      checked: this.props.checked
+    }
+    this.onHandleImageClick = this.onHandleImageClick.bind(this)
+  }
+
+  componentDidUpdate(prevProps) {
+    if(prevProps.checked != this.props.checked) {
+      this.setState({
+        checked: !prevProps.checked
+      })
+    }
+  }
+
+  onHandleImageClick() {
+    this.setState({
+      checked: !this.state.checked,
+    })
+  }
+
+  render() {
+    let borderColor = this.state.checked ? '#000000' : '#FFFFFF'
+    return (
+      <div style={{display : 'inline'}}>
+      <img onClick={this.onHandleImageClick} style={{ borderColor: borderColor , borderWidth : '5px', borderStyle: 'solid', height : 60, width : 60, marginLeft : 30, marginTop : 10}} src={this.state.url}/>
+      </div>
+    )
+  }
+}
+
+class ImageHolderList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { 
+      urls : this.props.urls,
+      checked: this.props.checked
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if(prevProps.checked != this.props.checked) {
+      this.setState({
+        checked: !prevProps.checked
+      })
+    }
+  }
+
+  render() {
+    console.log(this.state.checked)
+    return (
+      <div>
+      {this.state.urls.map(url => (<ImageHolder key={url} url={url} checked={this.state.checked}></ImageHolder>))}
+      </div>
+    )
+  }
+}
+
+class NSFWCategory extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      name : this.props.name,
+      urls: this.props.urls,
+      checked: false,
+    }
+    this.handleCheckboxChange = this.handleCheckboxChange.bind(this)
+  } 
+  handleCheckboxChange(e) {
+    let checked = !this.state.checked
+    this.setState({
+      checked: checked
+    })
+    
+  }
+
+  render() {
+    console.log(this.state.checked)
+    return (
+      <div>
+        <h2>{this.state.name}</h2>
+        <input type="checkbox" checked={this.state.checked} onChange={this.handleCheckboxChange}/> select all
+        <ImageHolderList urls={this.state.urls || []} checked={this.state.checked}></ImageHolderList>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{marginLeft : 30}}>
+        {data.map(cat => (<NSFWCategory key={cat["title"]} name={cat["title"]} urls={cat["urls"]}></NSFWCategory>))}
+        <div style={{marginTop: 30}}><button style={{height: 45, width: 80, background: '#546fff', color: '#ffffff', fontSize: 14}}>Upload</button></div>
       </div>
     );
   }
