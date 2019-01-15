29e5172ed6981854815845a2f40e73ffcbb50e2b
Rushabh Nagda <rushabhnagda11@gmail.com>
2019-01-15 13:24:21 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pd-nsfw/precache-manifest.c05870de4252fb4ab64419c98a7576b0.js"
+  "/pd-nsfw/precache-manifest.2a2e71776974311f71e64e0d58be3cf7.js"
 );
 
 workbox.clientsClaim();
